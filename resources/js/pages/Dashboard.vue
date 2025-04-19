1c477f5601713a44cb0e82511ecc32d461093775
hamidalem <hamidpro198@gmail.com>
2025-04-19 14:10:37 +0100

    moumen

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -15,6 +15,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
     <Head title="Dashboard" />
+    <h1>moumen</h1>
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
